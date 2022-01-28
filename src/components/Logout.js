d2626b7a339e21fd6fe3962a74d4e722508076e0
Sharon Sanchez <sstharakan@gmail.com>
2022-01-28 15:54:14 -0600

    added http request into logout of the application

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect }from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
+const Logout = (props) => {     
+    useEffect(() => {
+        axiosWithAuth()
+        .post('/logout')
+        .then(res => {
+            localStorage.removeItem("token")
+            props.history.push('/login')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
     return(<div></div>);
 }
 
